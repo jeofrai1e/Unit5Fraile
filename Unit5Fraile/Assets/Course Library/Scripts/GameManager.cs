4406777c9745a0cbbb29d64aa12275d9f3f1f554
jeofrai1e <jeorocks323@gmail.com>
2023-04-18 11:38:26 -0700

    ase
    asdasd

---
+++ Unit5Fraile/Assets/Course Library/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    private float spawnRate = 0.25f ;
+    // Start is calledbefore the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+}
